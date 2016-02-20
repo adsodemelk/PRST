51814be3d8ff604d3a6d08c796c6de36f222550a
Andreas Røssland <andreas.roessland@gmail.com>
2016-02-20 19:43:34 +0100

    Add setup.cfg and setup.py for PyPI

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name = "PRST",
+    packages = ["PRST"],
+    version = "0.0.1",
+    description = "Python Reservoir Simulation Toolbox",
+    author = "Andreas Røssland",
+    author_email = "andreas.roessland@gmail.com",
+    url = "https://github.com/roessland/PRST",
+    download_url = "https://github.com/roessland/PRST/tarball/0.0.1",
+    keywords = ["MRST", "reservoir", "simulation", "PDEs"],
+    classifiers = [],
+)
